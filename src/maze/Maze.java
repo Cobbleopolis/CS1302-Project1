5f3b523b223bfecf91596fe5288b0012fc140143
Cobbleopolis <cobbleopolis@gmail.com>
2017-02-21 19:11:16 -0500

    Added direction enum and directional debug printing

--- src/maze/Maze.java
+++ src/maze/Maze.java
@@ -7,26 +7,27 @@ package maze;
 
 public class Maze {
 
+    //TODO remove before final turn in?
+    static final private boolean DEBUG_PRINT = true;
     private int[][] arrMaze;
-    private char direction;
+    private Direction direction;
     private int r;  // x position of the mouse
     private int c;  //y position of the mouse
     private boolean exitFound = false;
-
-
 
 
     public Maze(int[][] arrMaze) {
         this.arrMaze = arrMaze;
         r = 0;
         c = this.arrMaze.length - 1;
-     }
+        direction = Direction.NORTH;
+    }
 
     //Prints out the maze without solution
     public void displayMaze() {
         for (int i = 0; i < arrMaze.length; i++) {
             for (int j = 0; j < arrMaze[i].length; j++) {
-                System.out.print(getDispChar(j, i, false) + " ");
+                System.out.print(getDispChar(j, i, false, DEBUG_PRINT) + " ");
             }
             System.out.println();
         }
@@ -36,7 +37,7 @@ public class Maze {
     public void displayPath() {
         for (int i = 0; i < arrMaze.length; i++) {
             for (int j = 0; j < arrMaze[i].length; j++) {
-                System.out.print(getDispChar(j, i, true) + " ");
+                System.out.print(getDispChar(j, i, true, DEBUG_PRINT) + " ");
             }
             System.out.println();
         }
@@ -82,25 +83,51 @@ public class Maze {
     }
 
     /**
-     * Gets the character to print for the specified x and y coordinates in the array.
-     * @param x The x location of the location to get the character for.
-     * @param y The y location of the location to get the character for.
+     * Gets the character to print for the specified x and y coordinates in the array. This a will not include any debug output.
+     * @param x            The x location of the location to get the character for.
+     * @param y            The y location of the location to get the character for.
      * @param showSolution A boolean determining if the solution for the maze should be shown.
      * @return The appropriate character to print for the specified x and y location
      */
-    private char getDispChar(int x, int y, boolean showSolution) {
+    private String getDispChar(int x, int y, boolean showSolution) {
+        return getDispChar(x, y, showSolution, false);
+    }
+
+    /**
+     * Gets the character to print for the specified x and y coordinates in the array.
+     * @param x            The x location of the location to get the character for.
+     * @param y            The y location of the location to get the character for.
+     * @param showSolution A boolean determining if the solution for the maze should be shown.
+     * @param debug        A boolean determining if the debug output should be shown.
+     * @return The appropriate character to print for the specified x and y location
+     */
+    private String getDispChar(int x, int y, boolean showSolution, boolean debug) {
         int mazeVal = this.arrMaze[y][x];
         if (x == r && y == c)
-            return '@';
+            if (debug)
+                switch (direction) {
+                    case NORTH:
+                        return new StringBuilder().appendCodePoint(0x1F839).toString();
+                    case EAST:
+                        return new StringBuilder().appendCodePoint(0x1F83A).toString();
+                    case WEST:
+                        return new StringBuilder().appendCodePoint(0x1F838).toString();
+                    case SOUTH:
+                        return new StringBuilder().appendCodePoint(0x1F83B).toString();
+                    default:
+                        return "@";
+                }
+            else
+                return "@";
         else if (mazeVal == 0)
-            return '#';
+            return "#";
         else if (mazeVal == 1)
-            return ' ';
+            return " ";
         else if (mazeVal == 2)
-            return '~';
+            return "~";
         else if (mazeVal == 3)
-            return (showSolution) ? '*' : ' ';
+            return (showSolution) ? "*" : " ";
         else
-            return '?';
+            return "?";
     }
 }
